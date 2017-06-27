4b2400bc588fd027e6143d3c07cf9fefa8be5332
il86 <il86@users.noreply.github.com>
2017-06-27 12:17:37 +0500

    Commit_08

--- src/UI/general/jqueryUI.js
+++ src/UI/general/jqueryUI.js
@@ -1,15 +1,15 @@
 function lbcJQueryEffects() {
     if ("function" != typeof $.prototype.effect) {
-        console.log("LOADING JQUERY EFFECT STUFF");
-        
+        console.log("LOADING JQUERY..");
         var a = document.createElement("script");
         a.async = !1;
         a.src = _prot + "//code.jquery.com/jquery-1.12.4.js";
         document.body.appendChild(a)
         
-        a = document.createElement("script");
-        a.async = !1;
-        a.src = _prot + "//code.jquery.com/ui/1.12.1/jquery-ui.js";
-        document.body.appendChild(a)
+        console.log("LOADING JQUERY EFFECT STUFF..");
+        var b = document.createElement("script");
+        b.async = !1;
+        b.src = _prot + "//code.jquery.com/ui/1.12.1/jquery-ui.js";
+        document.body.appendChild(b)
     }
 };
