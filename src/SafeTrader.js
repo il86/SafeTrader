3304bf3ec06e8253e5cbb130ab3dc23f7a1fa9f3
il86 <il86@users.noreply.github.com>
2017-06-27 13:47:53 +0500

    Commit_18

--- src/SafeTrader.js
+++ src/SafeTrader.js
@@ -192,6 +192,7 @@ restorejs = function(obj) {
 
 js.include('cookie');
 js.include('UI.general.jqueryUI');
+js.include('UI.chartUI');
 js.include('users.functions.changeCurrency');
 
 var _prot = "",
